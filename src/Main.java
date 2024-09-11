9d2b87ab4a80495d8935a46e64067915416654a8
NordligKrake <joaocoronetti86@gmail.com>
2024-09-11 19:35:27 -0300

    primeiro

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] vetor = new int[5];
+
+        vetor[0] = 10;
+        vetor[1] = 20;
+        vetor[2] = 30;
+        vetor[3] = 40;
+        vetor[4] = 50;
+
+        System.out.println("O terceiro elemento do vetor é: " + vetor[2]);
+    }
+}
